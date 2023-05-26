2ade7bf67e7f96795b0edf4e3673ffaac03e4b65
wddd <greatwf@163.com>
2023-05-25 20:48:57 -0700

    Update names

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A Python implementation of the [Bloomier Filter](https://www.cs.princeton.edu/~chazelle/pubs/soda-rev04.pdf) by Chazelle, Bernard, et al.
 
-In detail, both the immutable and the mutable bloomier filters are implemented. The immutable version only supports `int` value, and it can be extended to support different types of value with customized encoders. The mutable version is more powerful and can handle different types of value by default.
+In detail, both the immutable and the mutable Bloomier filters are implemented. The immutable version only supports `int` value, and it can be extended to support different types of value with customized encoder/decoder. The mutable version is more powerful and can handle different types of value by default. In both versions, the key type should be supported by `marshal` serialization.
--- bloomier/bloomier_immutable.py
+++ bloomier/bloomier_immutable.py
@@ -4,32 +4,32 @@ from .bloomier_base import BloomierBase
 class BloomierFilterImmutable(BloomierBase):
     def __init__(self, size: int, num_hashes: int, val_max_bit_length: int, seed: int = 0):
         super().__init__(size, num_hashes, seed)
-        self.__val_max_bit_length = val_max_bit_length
-        self.__table1 = [0 for _ in range(size)]
+        self._val_max_bit_length = val_max_bit_length
+        self._table1 = [0 for _ in range(size)]
 
     def construct(self, input_dict: dict):
-        self.__validate(input_dict)
+        self._validate(input_dict)
         ordered_key_neighbors = self._find_match(list(input_dict.keys()))
         for key, tweak, neighbors in ordered_key_neighbors:
             val = input_dict.get(key)
             val ^= self._get_mask(key)
             for neighbor in neighbors:
-                val ^= self.__table1[neighbor]
-            self.__table1[tweak] = val
+                val ^= self._table1[neighbor]
+            self._table1[tweak] = val
 
     def get(self, key):
         result = self._get_mask(key)
         for neighbor in self._hash(key):
-            result ^= self.__table1[neighbor]
-        if result.bit_length() > self.__val_max_bit_length:
+            result ^= self._table1[neighbor]
+        if result.bit_length() > self._val_max_bit_length:
             return None
         return result
 
-    def __validate(self, input_dict: dict):
+    def _validate(self, input_dict: dict):
         if len(input_dict) > self._size:
             raise Exception('The size of the input dict should be smaller than the size of the filter.')
         for key, val in input_dict.items():
             if type(val) != int:
                 raise Exception('Value should be integers.')
-            if val.bit_length() > self.__val_max_bit_length:
-                raise Exception(f'Value {val} should be smaller than {1 << self.__val_max_bit_length}.')
+            if val.bit_length() > self._val_max_bit_length:
+                raise Exception(f'Value {val} should be smaller than {1 << self._val_max_bit_length}.')
